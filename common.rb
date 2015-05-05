8aff63fae61cd4471a7b1f21ca068932922404eb
Brandur <brandur@mutelight.org>
2015-05-05 10:07:52 -0700

    Add more metrics

--- common.rb
+++ common.rb
@@ -12,3 +12,26 @@ class DummyJob < Que::Job
     sleep 0.01
   end
 end
+
+class Metrics
+  PREFIX = "que-degradation-test"
+
+  def self.count(name)
+    puts "count##{PREFIX}.#{name}"
+  end
+
+  def self.measure(name, value = nil)
+    t = Time.now
+    v = if block_given?
+      yield
+    else
+      raise ArgumentError, "need value without block" unless value
+      value
+    end
+    puts "measure##{PREFIX}.#{name}=#{v}s"
+  end
+
+  def self.sample(name, value, units)
+    puts "sample##{PREFIX}.#{name}=#{value}#{units}"
+  end
+end
